a4f564a9e64b0b414657a022a0def7dd9cd92f4b
JasMintTea <122884253+JasMintTea@users.noreply.github.com>
2026-03-04 02:02:57 -0400

    Complete HR dashboard with add participant, CSS Styling and participation logic

--- App/controllers/hr_controller.py
+++ App/controllers/hr_controller.py
@@ -4,16 +4,22 @@ from App.database import db
 
 
 def get_hr_stats(institution_id):
-    total_reg=Participant.query.filter_by(institution_id=institution_id).count()
-    participated = db.session.query(Registration)\
-        .join(Participant)\
+    total_participants=Participant.query.filter_by(institution_id=institution_id).count()
+    participated = db.session.query(Participant)\
+        .join(Registration)\
+        .join(Result)\
         .filter(Participant.institution_id == institution_id)\
-        .join(Result).distinct(Registration.participant_id).count()
+        .distinct(Registration.participant_id).count()
+    
     participants=Participant.query.filter_by(institution_id=institution_id).all()
+
+    for p in participants:
+        p.has_result = any(r.results for r in p.registrations)
+
     return {
-        "reg_count": total_reg,
+        "reg_count": total_participants,
         "part_count": participated,
-        "no_show_count": total_reg - participated,
+        "no_show_count": total_participants - participated,
         "participants": participants,
         "institution": Institution.query.get(institution_id)
     }
